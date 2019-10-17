3e26df9419d408c721cc9a1dacb9471fcc2a6bcd
Adam El-Masri <adam.el.masri@blockchainmarkets.com>
2019-10-17 18:13:48 -0400

    build config

---
+++ client/build/precache-manifest.45a5cad140e567fd0cb445e844a2df67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0a45c8a14f90cf7766",
+    "url": "/static/css/main.6f56f8ca.chunk.css"
+  },
+  {
+    "revision": "9e0a45c8a14f90cf7766",
+    "url": "/static/js/main.d52bba8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e0ecd5b1aef65dac07ed",
+    "url": "/static/js/2.3333d970.chunk.js"
+  },
+  {
+    "revision": "8d2447b6fb4badb5e91fe60705004c00",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds335668.mlab.com:35668/heroku_phx03123");
 
 // Start the API server
 app.listen(PORT, function() {
